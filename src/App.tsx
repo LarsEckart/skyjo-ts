7270141fd338d86ec87a6ac422a852855f50d49c
Lars Eckart <lars.eckart@hey.com>
2024-07-01 14:14:42 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-    const startGame = () => {
-        console.log("Game started");
+  const startGame = () => {
+    console.log("Game started");
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({}),
     };
+    fetch(
+      "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games",
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
   return (
     <div className="App">
       <header className="App-header"></header>
