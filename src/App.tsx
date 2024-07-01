500a96f837475b83db81f491751e4430fea1528f
Lars Eckart <lars.eckart@hey.com>
2024-07-01 14:38:05 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 
 function App() {
-  const [players, setPlayers] = useState('');
+  const [players, setPlayers] = useState("");
   const [gameId, setGameId] = useState(null);
 
   useEffect(() => {
-      const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({}),
-      };
-      fetch(
-          "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games",
-          requestOptions,
-      )
-          .then((response) => response.json())
-          .then((data) => {
-              setGameId(data.id);
-              console.log(data.id);
-          });
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({}),
+    };
+    fetch(
+      "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games",
+      requestOptions,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setGameId(data.id);
+        console.log(data.id);
+      });
   }, []);
 
   const startGame = () => {
     console.log("Game started", players);
     if (gameId) {
-        // TODO get player names
-        const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({}),
-        };
-        fetch(
-            "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games",
-            requestOptions,
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                // TODO
-            });
+      // TODO get player names
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: "player1",
+        }),
+      };
+      const registerPlayerUrl =
+        "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games/" +
+        gameId +
+        "/players";
+      fetch(registerPlayerUrl, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
     }
   };
 
