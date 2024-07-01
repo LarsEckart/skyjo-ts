03b5a9a57b4918ccdc67b69ade4b959fa02c0958
Leelo Rand <leelo.rand@gmail.com>
2024-07-01 14:34:25 +0300

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import "./App.css";
 
 function App() {
   const [players, setPlayers] = useState('');
+  const [gameId, setGameId] = useState(null);
+
   useEffect(() => {
       const requestOptions = {
           method: "POST",
@@ -15,10 +17,30 @@ function App() {
           requestOptions,
       )
           .then((response) => response.json())
-          .then((data) => console.log(data.id));
+          .then((data) => {
+              setGameId(data.id);
+              console.log(data.id);
+          });
   }, []);
+
   const startGame = () => {
     console.log("Game started", players);
+    if (gameId) {
+        // TODO get player names
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({}),
+        };
+        fetch(
+            "https://play-skyjo-ae5db0018a23.herokuapp.com/api/v1.0/skyjo/games",
+            requestOptions,
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                // TODO
+            });
+    }
   };
 
   return (
